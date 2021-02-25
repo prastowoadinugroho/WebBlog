7050869d2e33cbec7386de790ed59bfbd7425ba5
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-02-25 19:41:47 +0700

    Setup Server
    Setup node with import not const
    Node version must 13^
    Mongoose version 5.11.15

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+const app = express();
+
+dotenv.config({
+    path: './config/index.env'
+})
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(morgan('dev'));
+app.use(cors());
+
+const CONNECTION_URL = process.env.DB_URL;
+
+const PORT = process.env.PORT;
+
+mongoose.connect(CONNECTION_URL, 
+    {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+        .catch((err) => {
+            console.log(err.message)
+        })
+mongoose.set('useFindAndModify', false)
