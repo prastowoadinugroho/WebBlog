7aec79eba26ae43e49a912592b5f34d617747bb3
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-02-26 16:00:59 +0700

    Get and Add Post

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
@@ -14,7 +15,10 @@ dotenv.config({
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(morgan('dev'));
+
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.DB_URL;
 
