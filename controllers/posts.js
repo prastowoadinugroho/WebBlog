37022acd6b23b5f3cffc543ef03c59959e3baebf
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-03-01 15:45:02 +0700

    Edit post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req,res) => {
     try {
@@ -26,4 +27,15 @@ export const createPost = async (req,res) => {
             message: error.message
         })
     }
+}
+
+export const updatePost = async (req,res) => {
+    const {id: _id} = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('Post not found');
+
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+    
+    res.json(updatePost);
 }
