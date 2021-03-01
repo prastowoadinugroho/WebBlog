26dff98f60d3c8ff07d902be8deaa28d5b14033f
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-03-01 16:52:39 +0700

    Delete post

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,4 @@ const url = 'http://localhost:4000/posts';
 export const fetchPosts = () => Axios.get(url);
 export const createPost = (newPost) => Axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => Axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => Axios.delete(`${url}/${id}`);
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,12 +4,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
 
 import useStyles from './style';
+import { deletePost } from '../../../redux/actions/posts';
 
 const Post = ({post, setCurrentId}) => {
-
     const classes = useStyles();
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia 
@@ -41,8 +44,8 @@ const Post = ({post, setCurrentId}) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary">
-                    <DeleteIcon fontSize="small" onClick={()=> {}}/>
+                <Button size="small" color="primary" onClick={()=> dispatch(deletePost(post._id))}>
+                    <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
             </CardActions>
