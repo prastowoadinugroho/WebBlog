a82997f91fdc31e70788dba9b5731a509da55d6d
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-03-02 11:02:51 +0700

    Delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -39,3 +39,13 @@ export const updatePost = async (req,res) => {
     
     res.json(updatePost);
 }
+
+export const deletePost  = async (req,res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('Post not found');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully'});
+}
